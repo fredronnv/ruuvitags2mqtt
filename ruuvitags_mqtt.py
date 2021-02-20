2fa0aa1b1fda793f16b2072792b6f66eed33f25f
Fredrik Rönnvall <fredronn@gmail.com>
2021-02-20 17:30:44 +0100

    Update ruuvitags_mqtt.py
    Ability to update RUUVITAG_BASE_TOPIC

--- ruuvitags_mqtt.py
+++ ruuvitags_mqtt.py
@@ -18,6 +18,8 @@ mqtt_args = dict(
     bind_address=os.getenv('MQTT_BIND_ADDRESS', default="")
     )
 
+mqtt_base_topic = os.getenv('RUUVITAG_BASE_TOPIC', default='ruuvitags')
+
 mqttc.connect(mqtt_host, **mqtt_args)
 
 ruuvitags = {}
@@ -35,7 +37,7 @@ def msgify(name, payload):
     for key in payload.keys():
         if key != "identifier":
             try:
-                msgs.append({"topic": "ruuvitags/%s/%s" % (name,key), "payload": float(payload[key])})
+                msgs.append({"topic": "%s/%s/%s" % (mqtt_base_topic,name,key), "payload": float(payload[key])})
             except Exception as e:
                 pass
     return msgs
